fa7eb8f3017f42569e93775765616ed58dd691fe
HexPrism <bharathkrishnan33@gmail.com>
2023-09-13 21:44:55 +0530

    Fixed link issues due to global function

--- Fraglog.cpp
+++ Fraglog.cpp
@@ -43,3 +43,71 @@ FragLog::~FragLog()
     file << std::endl;
     file.close();
 }
+
+#ifdef _WIN32
+
+
+    #include <windows.h>
+    void FragLog::PrintColor(std::string line, ErrorCodes code,bool newLine) {
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        int col = 7;
+
+        switch(code)
+        {
+            case ErrorCodes::ERROR:
+                col = 4;
+                break;
+            case ErrorCodes::WARNING:
+                col = 6;
+                break;
+            case ErrorCodes::LOG:
+                col = 3;
+                break;
+            default:
+                col = 3;
+                break;
+        }
+
+        SetConsoleTextAttribute(hConsole, col);
+        std::cout << line;
+        if (newLine) {
+            std::cout << std::endl;
+        }
+        SetConsoleTextAttribute(hConsole, 7);
+    }
+
+
+#else
+
+
+
+    void FragLog::PrintColor(std::string line, ErrorCodes code,bool newLine) 
+    {
+        std::string col = "\033[0m";
+
+        switch(code)
+        {
+            case ErrorCodes::ERROR:
+                col = "\033[0;31m";
+                break;
+            case ErrorCodes::WARNING:
+                col = "\033[0;32m";
+                break;
+            case ErrorCodes::LOG:
+                col = "\033[0;36m";
+                break;
+            default:
+                col = "\033[0;36m";
+                break;
+        }
+
+
+        std::cout << col << line << "\033[0m";
+        if (newLine) {
+            std::cout << std::endl;
+        }
+    }
+
+
+#endif
+
--- Fraglog.hpp
+++ Fraglog.hpp
@@ -15,79 +15,11 @@ enum class ErrorCodes
 };
 
 
-#ifdef _WIN32
-
-
-    #include <windows.h>
-    void PrintColor(std::string line, ErrorCodes code=ErrorCodes::LOG,bool newLine = false) {
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        int col = 7;
-
-        switch(code)
-        {
-            case ErrorCodes::ERROR:
-                col = 4;
-                break;
-            case ErrorCodes::WARNING:
-                col = 6;
-                break;
-            case ErrorCodes::LOG:
-                col = 3;
-                break;
-            default:
-                col = 3;
-                break;
-        }
-
-        SetConsoleTextAttribute(hConsole, col);
-        std::cout << line;
-        if (newLine) {
-            std::cout << std::endl;
-        }
-        SetConsoleTextAttribute(hConsole, 7);
-    }
-
-
-#else
-
-
-
-    void PrintColor(std::string line, ErrorCodes code=ErrorCodes::LOG,bool newLine = false) 
-    {
-        std::string col = "\033[0m";
-
-        switch(code)
-        {
-            case ErrorCodes::ERROR:
-                col = "\033[0;31m";
-                break;
-            case ErrorCodes::WARNING:
-                col = "\033[0;32m";
-                break;
-            case ErrorCodes::LOG:
-                col = "\033[0;36m";
-                break;
-            default:
-                col = "\033[0;36m";
-                break;
-        }
-
-
-        std::cout << col << line << "\033[0m";
-        if (newLine) {
-            std::cout << std::endl;
-        }
-    }
-
-
-#endif
-
-
 
 
 
 class FragLog 
-    {
+{
     public:
         FragLog();
         void getStackTrace(unsigned int);
@@ -104,6 +36,7 @@ class FragLog
             file << "********************"<< std::endl;
             file << log << std::endl;   
         }
+        static void PrintColor(std::string line, ErrorCodes code=ErrorCodes::LOG,bool newLine = false);
         ~FragLog();
 
 
